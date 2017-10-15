6c58e2c5a9bd02c5aab14b857d2145dee2c2d9e6
saidmattar <ssabdou@gmail.com>
2017-10-14 17:07:18 -0700

    created a server.js file

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.static(`${__dirname}/build`));
 app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
-app.listen(process.env.PORT || 3000, () => console.log('SERVER RUNNING ON:', process.env.PORT));
+app.listen(process.env.PORT || 3000, () => console.log('SERVER RUNNING ON:', process.env.PORT)); 
