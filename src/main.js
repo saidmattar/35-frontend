3ed48988fed6ffa1319a2c1213d429fd3f1ccb1e
saidmattar <ssabdou@gmail.com>
2017-10-14 17:01:48 -0700

    scaffolded the folders and files

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './component/app';
+import {Provider} from 'react-redux';
+import appCreateStore from './lib/app-create-store';
+
+let store = appCreateStore();
+
+class AppContainer extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
